94ba19142d92f73467fb86521d317fa220cc5a97
Ben Summers <ben.summers@ib1.org>
2024-11-18 09:51:59 +0000

    Gather origins IDs from steps for consistency, check IDs match top level property when verifying

--- src/ib1/provenance/record.py
+++ src/ib1/provenance/record.py
@@ -37,15 +37,17 @@ class Record:
         self._require_signed()
         # Recursively verify record
         steps = []
+        origins = []
         signer_stack = []
         certificates_from_record = self._record.get("certificates")
         if certificates_from_record is None:
             certificates_from_record = {}
-        self._verify_record_container(self._record["steps"], certificates_from_record, certificate_provider, steps, signer_stack)
-        # TODO: Verify origins top level property is correct?
+        self._verify_record_container(self._record["steps"], certificates_from_record, certificate_provider, steps, origins, signer_stack)
+        if self._record["origins"] != origins:
+            raise Exception("origins property does not match origin steps in record")
         self._verified = steps
 
-    def _verify_record_container(self, container, certificates_from_record, certificate_provider, steps, signer_stack):
+    def _verify_record_container(self, container, certificates_from_record, certificate_provider, steps, origins, signer_stack):
         *data, sig_block = container
         container_format_version, serial, sign_timestamp, signature = sig_block
         # Check it's an understood format (multiple versions of formats may be included in a single record)
@@ -61,10 +63,12 @@ class Record:
         for e in data:
             if not isinstance(e, str):
                 signer_stack.append(signer_info)
-                self._verify_record_container(e, certificates_from_record, certificate_provider, steps, signer_stack)
+                self._verify_record_container(e, certificates_from_record, certificate_provider, steps, origins, signer_stack)
                 del signer_stack[-1]
             else:
                 decoded_step = json.loads(base64.urlsafe_b64decode(e))
+                if decoded_step["type"] == "origin":
+                    origins.append(decoded_step["id"])
                 decoded_step["_signature"] = {
                     "signed": signer_info,
                     "includedBy": copy.copy(signer_stack)
@@ -113,21 +117,16 @@ class Record:
 
     def sign(self, signer):
         output = []
-        origins = []
         certificates = {}
         if self._record is not None:
-            origins = self._record["origins"]
             if "certificates" in self._record:
                 certificates.update(self._record["certificates"])
             output.append(self._record["steps"]) # signed and encoded
         for r in self._additional_records:
-            origins.extend(r["origins"])
             if "certificates" in r:
                 certificates.update(r["certificates"])
             output.append(r["steps"]) # signed and encoded
         for s in self._additional_steps:
-            if s["type"] is "origin":
-                origins.append(s["id"])
             output.append(self._encode_step(s)) # unencoded, not signed
         serial = signer.serial()
         sign_timestamp = self._timestamp_now_iso8601()
@@ -151,6 +150,8 @@ class Record:
                 certificates[serial] = cert_path
                 for c in other_certs:
                     certificates[str(c.serial_number)] = [c.public_bytes(serialization.Encoding.PEM).decode("utf-8")]
+        origins = []
+        self._gather_origins(output, origins)
         encoded = {
             "ib1:provenance": self.trust_framework,
             "origins": origins,
@@ -159,6 +160,16 @@ class Record:
         if certificates:
             encoded["certificates"] = certificates
         return Record(self.trust_framework, encoded)
+
+    def _gather_origins(self, container, origins):
+        *data, sig_block = container
+        for e in data:
+            if not isinstance(e, str):
+                self._gather_origins(e, origins)
+            else:
+                decoded_step = json.loads(base64.urlsafe_b64decode(e))
+                if decoded_step["type"] == "origin":
+                    origins.append(decoded_step["id"])
 
     def _encode_step(self, step):
         return base64.urlsafe_b64encode(json.dumps(step, separators=(",", ":")).encode("utf-8")).decode('utf-8')
